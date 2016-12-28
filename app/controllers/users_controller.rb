ea18d4a67c3db70ebbffb2aa4c0339bc6ceefaa7
Hiroomi Kimura <643.cheesecake@gmail.com>
2016-12-28 11:22:36 +0000

    edit add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :user_match, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -18,9 +19,29 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to current_user , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :location, :bio)
+  end
+  
+  def user_match
+    if current_user.id != params[:id].to_i
+      redirect_to root_path
+    end
   end
 end
