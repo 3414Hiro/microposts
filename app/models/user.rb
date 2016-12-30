1d1634f4828e4d73e901a913ca3b8a5a80abc8c5
Hiroomi Kimura <643.cheesecake@gmail.com>
2016-12-30 09:46:27 +0000

    followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,36 @@ class User < ActiveRecord::Base
     
     has_many :microposts
     
+    has_many :following_relationships, class_name: "Relationship",
+                                       foreign_key: "follower_id",
+                                       dependent: :destroy
+                                       
+    has_many :following_users, through: :following_relationships, source: :followed
+    
+    has_many :follower_relationships, class_name: "Relationship",
+                                      foreign_key: "followed_id",
+                                      dependent: :destroy
+    
+    has_many :follower_users, through: :follower_relationships, source: :follower
+
+    # 他のユーザーをフォローする
+    def follow(other_user)
+        following_relationships.find_or_create_by(followed_id: other_user.id)
+    end
+    
+    # フォローしているユーザーをアンフォローする
+    def unfollow(other_user)
+        following_relationships = following_relationships.find_by(followed_id: other_user.id)
+        following_relationships.destroy if following_relationship
+    end
+    
+    # あるユーザーをフォローしているかどうか？
+    def following?(other_user)
+        following_users.include?(other_user)
+    end
+
+    
+    
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
